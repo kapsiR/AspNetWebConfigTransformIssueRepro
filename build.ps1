46da58692c3889378c05dbcef8dd01ac06610f4b
kapsiR <kapsiR@users.noreply.github.com>
2020-11-06 22:07:54 +0100

    Add restore packages to build script

--- build.ps1
+++ build.ps1
@@ -1,2 +1,3 @@
 $msbuildBin = (Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\" -Recurse -Filter "msbuild.exe" | Select-Object -First 1).FullName
+& $msbuildBin -t:restore
 & $msbuildBin .\WebConfigTransformRepro.sln /p:DeployOnBuild=true /p:Configuration=Release /p:PublishProfile=FolderProfile
